de2d6bb8059dbca6275ef6b8a9b3b52ea0e9af73
Jonathan Costa <jonathanduartecosta@gmail.com>
2022-10-07 20:25:12 -0300

    Modificado o cod html, css e js. Substitui o 'ul' pelo 'table' e feita as devidas atualizações no codigo

--- exercicio_jquery/main.js
+++ exercicio_jquery/main.js
@@ -14,12 +14,22 @@ $(document).ready(function () {
     const data = $("#data-conclusao").val();
 
     /*cria a nova linha adicionando o conteudo*/
-    const novaLinha = $(`<li>${novaTarefa} ${data}</li>`);
-
-    $(novaLinha).appendTo("ul");
+    const novaLinha = $(
+      `<tr><td>${novaTarefa}</td> <td>${dataFormatada(data)}</td></tr>`
+    );
+    $(novaLinha).appendTo("tbody");
 
     /*limpa os campos, deixando vazio*/
     $("#adiciona-tarefa").val("");
     $("#data-conclusao").val("");
   });
+
+  function dataFormatada(date) {
+    date = new Date();
+    let ano = date.getFullYear();
+    let mes = (1 + date.getMonth()).toString().padStart(2, "0");
+    let dia = date.getDate().toString().padStart(2, "0");
+
+    return dia + "-" + mes + "-" + ano;
+  }
 });
