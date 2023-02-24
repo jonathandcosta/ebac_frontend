828fe633ffc41a8a36194414933c289ac9642f78
Jonathan Costa <jonathanduartecosta@gmail.com>
2023-02-23 21:29:29 -0300

    refeito o codigo html e JS com melhorando o funcionamento

--- exercicio_ajax_git_hub/main.js
+++ exercicio_ajax_git_hub/main.js
@@ -1,12 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // ajax começa aqui
-  const xhttp = new XMLHttpRequest();
-  const repositorio = document.getElementById("repositorio").value;
-  const seguidores = document.getElementById("seguidores").value;
-  const seguindo = document.getElementById("seguindo").value;
+  //variaveis referenciando os ID's dos campos a serem alimentado pelos dados da API do github
 
-  const endpoint = `https://github.com/jonathandcosta`;
+  const nameElement = document.querySelector("#name");
+  const usernameElement = document.querySelector("#username");
+  const avatarElement = document.querySelector("#avatar");
+  const repositoriesElement = document.querySelector("#repos");
+  const followersElement = document.querySelector("#followers");
+  const followingElement = document.querySelector("#following");
+  const linkElement = document.querySelector("#link");
+  const endpoint = `https://api.github.com/users/jonathandcosta`;
 
-  xhttp.open("GET", endpoint);
-  xhttp.send();
+  fetch(endpoint)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (json) {
+      nameElement.innerText = json.name;
+      usernameElement.innerText = json.login;
+      avatarElement.src = json.avatar_url;
+      repositoriesElement.innerText = json.public_repos;
+      followersElement.innerText = json.followers;
+      followingElement.innerText = json.following;
+      linkElement.href = json.html_url;
+    })
+    .catch(function (erro) {
+      alert("ocorreu um erro");
+    });
 });
