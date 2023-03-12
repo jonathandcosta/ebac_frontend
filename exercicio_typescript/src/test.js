74ce0ccfa8068d0af3fa72739a90477e9c85aa7f
Jonathan Costa <jonathanduartecosta@gmail.com>
2023-03-12 20:45:29 -0300

    entrega do exercicio M25

---
+++ exercicio_typescript/src/test.js
@@ -0,0 +1,11 @@
+"use strict";
+// PRIMEIRA FUNÇÃO
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiply(2, 7));
+// SEGUNDA FUNÇÃO
+function saudacao(name) {
+    return `Olá ${name}!`;
+}
+console.log(saudacao("Jonathan"));
---
+++ exercicio_typescript/src/test.ts
@@ -0,0 +1,11 @@
+// PRIMEIRA FUNÇÃO
+function multiply(num1: number, num2: number): number {
+  return num1 * num2;
+}
+console.log(multiply(2, 7));
+
+// SEGUNDA FUNÇÃO
+function saudacao(name: string): string {
+  return `Olá ${name}!`;
+}
+console.log(saudacao("Jonathan"));
